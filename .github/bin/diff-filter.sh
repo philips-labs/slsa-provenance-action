542ab9fbad442dd37e990cce1ab465081f067ba4
Brend Smits <brend.smits@philips.com>
2021-10-04 15:17:11 +0200

    Add integration test for file provenance type

---
+++ .github/bin/diff-filter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FIRST_ARTIFACT=$1
+SECOND_ARTIFACT=$2
+# Comma seperated line of text of attributes to skip, examples are: buildFinishedOn,sha1
+# SKIP_LINES=$3
+
+diff_artifacts() {
+    diff -f $FIRST_ARTIFACT $SECOND_ARTIFACT > difference.txt
+    grep -vF "buildFinishedOn" difference.txt > difference2.txt
+    grep -vF "sha1" difference2.txt > difference3.txt
+    cat difference3.txt
+    # cat difference.txt | grep -vF "buildFinishedOn" | grep -vF "sha1" | grep -vF "buildInvocationId" | grep -vE "^c" | grep -vE "^."
+}
+
+diff_artifacts
