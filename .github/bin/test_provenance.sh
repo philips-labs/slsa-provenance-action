39289c04d72904f1af84ec73fe360d72f2da002a
Annie Jovitha <anniejovitha.arulanandam@philips.com>
2021-10-05 12:22:15 +0200

    Updated test provenance file and check
    Co-authored-by: Marco Franssen <marco.franssen@philips.com>

---
+++ .github/bin/test_provenance.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+FIRST_ARTIFACT=$1
+SECOND_ARTIFACT=$2
+
+diff_artifacts() {
+  local temp_file1=/tmp/file1
+  local temp_file2=/tmp/file2
+  (grep -v "buildInvocationId" | grep -v "buildFinishedOn" | grep -v "sha1") > $temp_file1 < "$FIRST_ARTIFACT"
+  (grep -v "buildInvocationId" | grep -v "buildFinishedOn" | grep -v "sha1") > $temp_file2 < "$SECOND_ARTIFACT"
+
+  diff -wf $temp_file1 $temp_file2 > /dev/null
+  local exit_code=$?
+  rm -rf /tmp/{file1,file2}
+  exit $exit_code
+}
+
+diff_artifacts
