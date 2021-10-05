a700ca0b2c73d7b424ec737520274ba99dae0637
anniejovitha.arulanandam <anniejovitha.arulanandam@gmail.com>
2021-10-05 21:37:02 +0200

    removed debug message

--- .github/bin/test_provenance.sh
+++ .github/bin/test_provenance.sh
@@ -4,17 +4,10 @@ FIRST_ARTIFACT=$1
 SECOND_ARTIFACT=$2
 
 diff_artifacts() {
-    echo "-----step1---------"
     local temp_file1=/tmp/file1
     local temp_file2=/tmp/file2
-    echo "-----step2---------"
-    (grep -v "buildInvocationId" | grep -vF "buildFinishedOn1") >$temp_file1 <"$FIRST_ARTIFACT"
-    (grep -v "buildInvocationId" | grep -vF "buildFinishedOn1") >$temp_file2 <"$SECOND_ARTIFACT"
-    echo "-----step3---------"
-    cat $temp_file1
-    echo "-----step4---------"
-    cat $temp_file2
-    echo "-----step5---------"
+    (grep -v "buildInvocationId" | grep -vF "buildFinishedOn") >$temp_file1 <"$FIRST_ARTIFACT"
+    (grep -v "buildInvocationId" | grep -vF "buildFinishedOn") >$temp_file2 <"$SECOND_ARTIFACT"
     diff -f $temp_file1 $temp_file2 >/dev/null
     local exit_code=$?
     rm -rf /tmp/{file1,file2}
