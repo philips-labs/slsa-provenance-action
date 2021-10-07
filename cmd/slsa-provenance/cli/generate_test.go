b79a22db919f486527f898a4e977eab1035e3b28
Brend Smits <brend.smits@philips.com>
2021-10-07 12:18:58 +0200

    Add unit tests for additional arguments
    Passing an argument to the Generate command is not funcational yet, leading to failed tests
    
    Signed-off-by: Brend Smits <brend.smits@philips.com>

--- cmd/slsa-provenance/cli/generate_test.go
+++ cmd/slsa-provenance/cli/generate_test.go
@@ -7,19 +7,49 @@ import (
 	"github.com/philips-labs/slsa-provenance-action/cmd/slsa-provenance/cli"
 )
 
-const (
-	ArtifactPathError = "no value found for required flag: -artifact_path"
-)
-
 func TestErrors(t *testing.T) {
-	cli := cli.Generate()
-	err := cli.Exec(context.Background(), make([]string, 0))
-
-	if err == nil {
-		t.Error("Expected an error but did not generate one")
-	} else {
-		if err.Error() != ArtifactPathError {
-			t.Errorf("Expected error to match: %s", ArtifactPathError)
-		}
+	testCases := []struct {
+		name          string
+		errorMessage  string
+		validArgument string
+	}{
+		{
+			name:          "test artifact path argument error",
+			errorMessage:  "no value found for required flag: -artifact_path",
+			validArgument: "-artifact_path artifact/path",
+		},
+		{
+			name:          "test output path argument error",
+			errorMessage:  "no value found for required flag: -output_path",
+			validArgument: "-output_path output/path",
+		},
+		{
+			name:          "test github context argument error",
+			errorMessage:  "no value found for required flag: -github_context",
+			validArgument: "-github_context gh-context",
+		},
+		{
+			name:          "test runner context argument error",
+			errorMessage:  "no value found for required flag: -runner_context",
+			validArgument: "-runner_context runner-context",
+		},
 	}
+
+	supplementedArguments := []string{}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			cli := cli.Generate()
+			err := cli.Exec(context.Background(), supplementedArguments)
+			supplementedArguments = append(supplementedArguments, tt.validArgument)
+
+			if err == nil {
+				t.Error("Expected an error but did not generate one")
+			} else {
+				if err.Error() != tt.errorMessage {
+					t.Errorf("Expected error to match: %s", tt.errorMessage)
+				}
+			}
+		})
+	}
+
 }
