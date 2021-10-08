ed00d023cebe1103350fa580ebfce5d10462227a
Brend Smits <brend.smits@philips.com>
2021-10-08 15:41:09 +0200

    Add more flexible arguments that allow for both happy and bad flow test cases
    Signed-off-by: Brend Smits <brend.smits@philips.com>

--- cmd/slsa-provenance/cli/generate_test.go
+++ cmd/slsa-provenance/cli/generate_test.go
@@ -7,46 +7,81 @@ import (
 	"github.com/philips-labs/slsa-provenance-action/cmd/slsa-provenance/cli"
 )
 
+type Arguments struct {
+	argument string
+	value    string
+}
+
 func TestErrors(t *testing.T) {
 	testCases := []struct {
-		name          string
-		errorMessage  string
-		validArgument []string
+		name         string
+		errorMessage string
+		arguments    []Arguments
 	}{
 		{
 			name:         "test artifact path argument error",
 			errorMessage: "no value found for required flag: -artifact_path",
-			// TODO: Refactor from validArguments to "Arguments" - Allows testing for bad/happy flow
-			validArgument: []string{"-artifact_path", "artifact/path"},
+			arguments: []Arguments{
+				{argument: "", value: ""},
+			},
 		},
 		{
-			name:          "test output path argument error",
-			errorMessage:  "no value found for required flag: -output_path",
-			validArgument: []string{"-output_path", "output/path"},
+			name:         "test github context argument error",
+			errorMessage: "no value found for required flag: -github_context",
+			arguments: []Arguments{
+				{argument: "-artifact_path", value: "artifact/path"},
+			},
 		},
 		{
-			name:          "test github context argument error",
-			errorMessage:  "no value found for required flag: -github_context",
-			validArgument: []string{"-github_context", "gh-context"},
+			name:         "test output path argument error",
+			errorMessage: "no value found for required flag: -output_path",
+			arguments: []Arguments{
+				{argument: "-artifact_path", value: "artifact/path"},
+				{argument: "-github_context", value: "gh-context"},
+			},
 		},
 		{
-			name:          "test runner context argument error",
-			errorMessage:  "no value found for required flag: -runner_context",
-			validArgument: []string{"-runner_context", "runner-context"},
+			name:         "test runner context argument error",
+			errorMessage: "no value found for required flag: -runner_context",
+			arguments: []Arguments{
+				{argument: "-artifact_path", value: "artifact/path"},
+				{argument: "-github_context", value: "gh-context"},
+				{argument: "-output_path", value: "output/path"},
+			},
 		},
-		// TODO: Add test case that provides no arguments (use make slice function)
+		{
+			name:         "test happy flow",
+			errorMessage: "",
+			arguments: []Arguments{
+				{argument: "-artifact_path", value: "artifact/path"},
+				{argument: "-github_context", value: "gh-context"},
+				{argument: "-output_path", value: "output/path"},
+				{argument: "-runner_context", value: "runner-context"},
+			},
+		},
+		{
+			name:         "test without arguments",
+			errorMessage: "",
+			arguments:    make([]Arguments, 0),
+		},
 	}
 
 	cli := cli.Generate()
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			err := cli.Parse(tc.validArgument)
+			args := []string{}
+			for _, v := range tc.arguments {
+				args = append(args, v.argument, v.value)
+			}
+			err := cli.Parse(args)
 			if err != nil {
 				t.Error("Failed parsing arguments")
 			}
 			err = cli.Run(context.Background())
 
-			// TODO: Happy flow: If err message is empty string, go test happy flow
+			if tc.errorMessage == "" {
+				// TODO: Happy flow: If err message is empty string, go test happy flow
+			}
 
 			if err == nil {
 				t.Error("Expected an error but did not generate one")
