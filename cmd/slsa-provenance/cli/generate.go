0502d05c560cc8800089abc7e4a75a35e0a8bac0
Marco Franssen <marco.franssen@philips.com>
2021-10-22 11:55:53 +0200

    Implement tagName flag on cli
    Using this new flag we can have the assets automatically downloaded
    from a release to generate the provenance for this release.
    
    Co-authored-by: Brend Smits <brend.smits@philips.com>

--- cmd/slsa-provenance/cli/generate.go
+++ cmd/slsa-provenance/cli/generate.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path"
+	"strings"
 
 	"github.com/peterbourgon/ff/v3/ffcli"
 	"github.com/pkg/errors"
@@ -23,7 +25,9 @@ func RequiredFlagError(flagName string) error {
 // Generate creates an instance of *ffcli.Command to generate provenance
 func Generate(w io.Writer) *ffcli.Command {
 	var (
-		flagset       = flag.NewFlagSet("slsa-provenance generate", flag.ExitOnError)
+		flagset = flag.NewFlagSet("slsa-provenance generate", flag.ExitOnError)
+		tagName = flagset.String("tag_name", "", `The github release to generate provenance on.
+(if set the artifacts will be downloaded from the release and the provenance will be added as an additional release asset.)`)
 		artifactPath  = flagset.String("artifact_path", "", "The file or dir path of the artifacts for which provenance should be generated.")
 		outputPath    = flagset.String("output_path", "build.provenance", "The path to which the generated provenance should be written.")
 		githubContext = flagset.String("github_context", "", "The '${github}' context value.")
@@ -65,6 +69,46 @@ func Generate(w io.Writer) *ffcli.Command {
 				return errors.Wrap(err, "failed to unmarshal runner context json")
 			}
 
+			if *tagName != "" {
+				ghToken := os.Getenv("GITHUB_TOKEN")
+				if ghToken == "" {
+					return fmt.Errorf("GITHUB_TOKEN environment variable not set")
+				}
+				tc := github.NewOAuth2Client(ctx, func() string { return ghToken })
+				pc := github.NewProvenanceClient(tc)
+
+				repoParts := strings.Split(gh.Repository, "/")
+				repo := repoParts[len(repoParts)-1]
+				rel, err := pc.FetchRelease(ctx, gh.RepositoryOwner, repo, *tagName)
+				if err != nil {
+					return err
+				}
+				assets, err := pc.DownloadReleaseAssets(ctx, gh.RepositoryOwner, repo, rel.GetID())
+				if err != nil {
+					return err
+				}
+				err = os.MkdirAll(*artifactPath, os.FileMode(os.O_RDWR))
+				if err != nil {
+					return err
+				}
+
+				for _, asset := range assets {
+					err := saveFile(path.Join(*artifactPath, asset.GetName()), asset.Content)
+					defer asset.Content.Close()
+					if err != nil {
+						return err
+					}
+				}
+
+				defer func() {
+					provenanceFile, err := os.Open(*outputPath)
+					if err != nil {
+						fmt.Printf("%s", err)
+					}
+					pc.AddProvenanceToRelease(ctx, gh.RepositoryOwner, repo, rel.GetID(), provenanceFile)
+				}()
+			}
+
 			stmt, err := slsa.GenerateProvenanceStatement(ctx, gh, runner, *artifactPath)
 			if err != nil {
 				return errors.Wrap(err, "failed to generate provenance")
@@ -83,4 +127,16 @@ func Generate(w io.Writer) *ffcli.Command {
 			return nil
 		},
 	}
+}
+
+func saveFile(path string, content io.ReadCloser) error {
+	assetFile, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer assetFile.Close()
+
+	_, err = io.Copy(assetFile, content)
+
+	return err
 }
