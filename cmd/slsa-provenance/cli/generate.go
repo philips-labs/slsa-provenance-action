920b639d20a4172b28eca1604440e69f7c6798a5
Pieter Lexis <pieter.lexis@powerdns.com>
2021-11-04 16:57:04 +0100

    Allow injection of extra materials via files
    Some build systems could create files that contain materials used during
    the build (like docker images, operating system packages etc.). These
    files would contain a JSON array with Items. When set on the
    command-line flag `-extra_materials`, slsa-provenance will read and
    parse these files and add the found materials to the statement.
    
    Signed-off-by: Pieter Lexis <pieter.lexis@powerdns.com>

--- cmd/slsa-provenance/cli/generate.go
+++ cmd/slsa-provenance/cli/generate.go
@@ -6,7 +6,9 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/peterbourgon/ff/v3/ffcli"
 	"github.com/pkg/errors"
@@ -26,11 +28,16 @@ func Generate(w io.Writer) *ffcli.Command {
 		flagset = flag.NewFlagSet("slsa-provenance generate", flag.ExitOnError)
 		tagName = flagset.String("tag_name", "", `The github release to generate provenance on.
 (if set the artifacts will be downloaded from the release and the provenance will be added as an additional release asset.)`)
-		artifactPath  = flagset.String("artifact_path", "", "The file or dir path of the artifacts for which provenance should be generated.")
-		outputPath    = flagset.String("output_path", "build.provenance", "The path to which the generated provenance should be written.")
-		githubContext = flagset.String("github_context", "", "The '${github}' context value.")
-		runnerContext = flagset.String("runner_context", "", "The '${runner}' context value.")
+		artifactPath   = flagset.String("artifact_path", "", "The file or dir path of the artifacts for which provenance should be generated.")
+		outputPath     = flagset.String("output_path", "build.provenance", "The path to which the generated provenance should be written.")
+		githubContext  = flagset.String("github_context", "", "The '${github}' context value.")
+		runnerContext  = flagset.String("runner_context", "", "The '${runner}' context value.")
+		extraMaterials = []string{}
 	)
+	flagset.Func("extra_materials", "Files that contain JSON encoded provenance to be included into the provenance", func(s string) error {
+		extraMaterials = append(extraMaterials, strings.Fields(s)...)
+		return nil
+	})
 
 	flagset.SetOutput(w)
 
@@ -78,6 +85,18 @@ func Generate(w io.Writer) *ffcli.Command {
 			stmt, err := env.GenerateProvenanceStatement(ctx, *artifactPath)
 			if err != nil {
 				return errors.Wrap(err, "failed to generate provenance")
+			}
+
+			for _, extra := range extraMaterials {
+				content, err := ioutil.ReadFile(extra)
+				if err != nil {
+					return errors.Wrapf(err, "Could not load extra materials from %s", extra)
+				}
+				var materials []intoto.Item
+				if err = json.Unmarshal(content, &materials); err != nil {
+					return errors.Wrapf(err, "Invalid JSON in extra materials file %s", extra)
+				}
+				stmt.Predicate.Materials = append(stmt.Predicate.Materials, materials...)
 			}
 
 			fmt.Fprintf(w, "Saving provenance to %s\n", *outputPath)
